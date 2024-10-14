2bf4ec4f35486c34d8eaae1204b3da7d852387e7
JovalierOrtiz <jovalier.ortiz@upr.edu>
2024-10-14 19:06:19 -0400

    ADD ROOM, MEETING DATA

--- schema.sql
+++ schema.sql
@@ -15,9 +15,16 @@ CREATE TABLE IF NOT EXISTS "section" (
 );
 
 CREATE TABLE IF NOT EXISTS "room" (
-
+    "rid" SERIAL PRIMARY KEY,
+    "building" VARCHAR(255),
+    "room_number" VARCHAR,
+    "capacity" INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS "meeting" (
-
+    "mid" SERIAL PRIMARY KEY,
+    "ccode" VARCHAR(10),
+    "starttime" TIMESTAMP,
+    "endtime" TIMESTAMP,
+    "cdays" VARCHAR(5) 
 );
