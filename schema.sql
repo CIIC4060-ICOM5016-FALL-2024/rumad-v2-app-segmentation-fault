b8d5c8e92ead53f39f5aa5b7b159f48525fa3b40
Onnelle Lugo <onnelle.lugo@upr.edu>
2024-10-14 18:47:51 -0400

    Add database schema for requisite, class, syllabus, section, room, and meeting tables

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS "requisite" (
+
+);
+
+CREATE TABLE IF NOT EXISTS "class" (
+
+);
+
+CREATE TABLE IF NOT EXISTS "syllabus" (
+
+);
+
+CREATE TABLE IF NOT EXISTS "section" (
+
+);
+
+CREATE TABLE IF NOT EXISTS "room" (
+
+);
+
+CREATE TABLE IF NOT EXISTS "meeting" (
+
+);
