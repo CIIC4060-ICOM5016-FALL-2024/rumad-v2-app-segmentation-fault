4a50bb645342e3895644ca5a4aaa9dba575736ad
JeanMontesSantiago <jean.montes@upr.edu>
2024-10-29 10:16:34 -0400

    adjust main to deploy in heroku

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
+
 
 from handler.section import SectionHandler
 
@@ -38,4 +40,4 @@ def requisite():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
