644b9bb53a9a2f77758b8ce57ab5183c8439c5fb
Onnelle Lugo <onnelle.lugo@upr.edu>
2024-11-02 21:32:34 -0400

    Implement update functionality for sections

--- app/app.py
+++ app/app.py
@@ -31,14 +31,14 @@ def sectionByID(sid):
     if request.method == "DELETE":
         return SectionHandler().deleteSectionBySid(sid)
     elif request.method == "PUT":
-        return "PUT"
+        return SectionHandler().updateSectionBySid(sid, request.json)
     else:
         return SectionHandler().getSectionBySid(sid)
 
 
-@app.route("/segmentation_fault/meeting", methods = ['GET', 'POST'])
+@app.route("/segmentation_fault/meeting", methods=["GET", "POST"])
 def meeting():
-    if request.method == 'GET':
+    if request.method == "GET":
         return MeetingHandler().getAllMeeting()
     else:
         return MeetingHandler().insertMeeting(request.json)
@@ -50,14 +50,15 @@ def getMeetingByMid(mid):
 
 
 # ROOM ROUTES
-@app.route("/segmentation_fault/room", methods=['GET', 'POST'])
+@app.route("/segmentation_fault/room", methods=["GET", "POST"])
 def room():
     if request.method == "GET":
         return RoomHandler().getAllRoom()
     else:
         return RoomHandler().insertRoom(request.json)
 
-@app.route("/segmentation_fault/room/<int:rid>", methods=['GET', 'PUT', 'DELETE'])
+
+@app.route("/segmentation_fault/room/<int:rid>", methods=["GET", "PUT", "DELETE"])
 def getRoomByRID(rid):
     if request.method == "GET":
         return RoomHandler().getRoomByRid(rid)
@@ -74,7 +75,8 @@ def courses():
         return ClassHandler().getAllClass()
     elif request.method == "POST":
         return ClassHandler().insertClass(request.json)
-    
+
+
 @app.route("/segmentation_fault/class/<int:cid>", methods=["GET", "PUT", "DELETE"])
 def courses2(cid):
     if request.method == "GET":
@@ -84,15 +86,19 @@ def courses2(cid):
     elif request.method == "DELETE":
         return ClassHandler().deleteClassById(cid)
 
-@app.route("/segmentation_fault/requisite", methods = ['GET', 'POST'])
+
+@app.route("/segmentation_fault/requisite", methods=["GET", "POST"])
 def requisite():
-    if request.method == 'GET':
+    if request.method == "GET":
         return RequisiteHandler().getAllRequisite()
     else:
         return RequisiteHandler().insertRequisite(request.json)
 
 
-@app.route("/segmentation_fault/requisite/<int:classid>/<int:reqid>", methods=["GET", "PUT", "DELETE"])
+@app.route(
+    "/segmentation_fault/requisite/<int:classid>/<int:reqid>",
+    methods=["GET", "PUT", "DELETE"],
+)
 def requisiteByClassIdReqId(classid, reqid):
     if request.method == "DELETE":
         return RequisiteHandler().deleteRequisiteByClassIdReqId(classid, reqid)
@@ -100,6 +106,7 @@ def requisiteByClassIdReqId(classid, reqid):
         return "PUT"
     else:
         return RequisiteHandler().getRequisiteByClassIdReqId(classid, reqid)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
