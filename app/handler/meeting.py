3ba05e6bd1f2aeb213084fe657637407d11707c5
Fernan2908 <fernando.sierra3@upr.edu>
2024-11-03 22:26:26 -0400

    Fix the update duplicates issue

--- app/handler/meeting.py
+++ app/handler/meeting.py
@@ -10,12 +10,8 @@ class MeetingHandler:
         result = {}
         result["mid"] = tuple[0]
         result["ccode"] = tuple[1]
-        result["starttime"] = (
-            tuple[2].strftime("%H:%M:%S") if hasattr(tuple[2], "strftime") else tuple[2]
-        )
-        result["endtime"] = (
-            tuple[3].strftime("%H:%M:%S") if hasattr(tuple[3], "strftime") else tuple[3]
-        )
+        result["starttime"] = (tuple[2].strftime("%H:%M:%S") if hasattr(tuple[2], "strftime") else tuple[2])
+        result["endtime"] = (tuple[3].strftime("%H:%M:%S") if hasattr(tuple[3], "strftime") else tuple[3])
         result["cdays"] = tuple[4]
         return result
 
@@ -26,9 +22,7 @@ class MeetingHandler:
 
         # Check if the data to insert is already in the database
         values_to_check = df_to_verify[columns_to_check].iloc[0]
-        duplicate_count = (
-            df_meeting[columns_to_check].eq(values_to_check).all(axis=1).sum()
-        )
+        duplicate_count = (df_meeting[columns_to_check].eq(values_to_check).all(axis=1).sum())
 
         return duplicate_count == 1
 
@@ -51,21 +45,13 @@ class MeetingHandler:
             return "Not Found", 404
 
     def insertMeeting(self, meeting_json):
-        if (
-            "ccode" not in meeting_json
-            or "starttime" not in meeting_json
-            or "endtime" not in meeting_json
-            or "cdays" not in meeting_json
-        ):
+        if ("ccode" not in meeting_json or "starttime" not in meeting_json 
+            or "endtime" not in meeting_json or "cdays" not in meeting_json):
             return "Missing required fields", 400
 
         ccode = meeting_json["ccode"]
-        starttime = datetime.strptime(meeting_json["starttime"], "%H:%M:%S").strftime(
-            "%H:%M:%S"
-        )
-        endtime = datetime.strptime(meeting_json["endtime"], "%H:%M:%S").strftime(
-            "%H:%M:%S"
-        )
+        starttime = datetime.strptime(meeting_json["starttime"], "%H:%M:%S").strftime("%H:%M:%S")
+        endtime = datetime.strptime(meeting_json["endtime"], "%H:%M:%S").strftime("%H:%M:%S")
         cdays = meeting_json["cdays"]
 
         data = {
@@ -93,37 +79,46 @@ class MeetingHandler:
             return self.mapToDict(temp), 201
 
         else:
-            return (
-                "Data can't be inserted due to duplicates or record already exists",
-                400,
-            )
+            return ("Data can't be inserted due to duplicates or invalid data", 400,)
 
     def updateMeetingByMid(self, mid, meeting_json):
         dao = MeetingDAO()
         if not dao.getMeetingByMid(mid):
             return "Not Found", 404
 
-        if (
-            "ccode" not in meeting_json
-            or "starttime" not in meeting_json
-            or "endtime" not in meeting_json
-            or "cdays" not in meeting_json
-        ):
+        if ( "ccode" not in meeting_json or "starttime" not in meeting_json or 
+            "endtime" not in meeting_json or "cdays" not in meeting_json):
             return "Missing required fields", 400
 
         ccode = meeting_json["ccode"]
-        starttime = datetime.strptime(meeting_json["starttime"], "%H:%M:%S").strftime(
-            "%H:%M:%S"
-        )
-        endtime = datetime.strptime(meeting_json["endtime"], "%H:%M:%S").strftime(
-            "%H:%M:%S"
-        )
+        starttime = datetime.strptime(meeting_json["starttime"], "%H:%M:%S").strftime("%H:%M:%S")
+        endtime = datetime.strptime(meeting_json["endtime"], "%H:%M:%S").strftime("%H:%M:%S")
         cdays = meeting_json["cdays"]
+        
+        data = {
+            "mid": [mid],
+            "ccode": [ccode],
+            "starttime": [starttime],
+            "endtime": [endtime],
+            "cdays": [cdays],
+        }
+        df_to_update = pd.DataFrame(data)
+        df_list = clean_data(df_to_update, "meeting")
+        
+        df_meeting = []
+        for df, df_name in df_list:
+            if df_name == "meeting":
+                df_meeting = df
+                
+        is_data_confirmed = self.confirmDataInDF(df_to_update, df_meeting)
 
-        dao.updateMeetingByMid(mid, ccode, starttime, endtime, cdays)
-        temp = (mid, ccode, starttime, endtime, cdays)
+        if is_data_confirmed:
+            dao.updateMeetingByMid(mid, ccode, starttime, endtime, cdays)
+            temp = (mid, ccode, starttime, endtime, cdays)
 
-        return self.mapToDict(temp), 200
+            return self.mapToDict(temp), 200
+        else:
+            return ("Data can't be inserted due to duplicates or invalid data", 400,)
 
     def deleteMeetingByMid(self, mid):
         dao = MeetingDAO()
