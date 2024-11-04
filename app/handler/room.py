943503d1d06d495fbe3a65826f9b0aa9f35e2484
Fernan2908 <fernando.sierra3@upr.edu>
2024-11-03 23:30:36 -0400

    Fix the insert duplicates issue

--- app/handler/room.py
+++ app/handler/room.py
@@ -1,5 +1,8 @@
+import pandas as pd
 from flask import jsonify
 from dao.room import RoomDAO
+from handler.data_validation import clean_data
+
 
 
 class RoomHandler:
@@ -11,6 +14,17 @@ class RoomHandler:
             "capacity" : tuple[3]
         }
         return result
+    
+    def confirmDataInDF(self, df_to_verify, df_meeting):
+        columns_to_check = ["building", "room_number", "capacity"]
+        df_meeting = df_meeting.astype({col: str for col in columns_to_check})
+        df_to_verify = df_to_verify.astype({col: str for col in columns_to_check})
+
+        # Check if the data to insert is already in the database
+        values_to_check = df_to_verify[columns_to_check].iloc[0]
+        duplicate_count = (df_meeting[columns_to_check].eq(values_to_check).all(axis=1).sum())
+
+        return duplicate_count == 1
 
     def getAllRoom(self):
         result = []
@@ -26,16 +40,39 @@ class RoomHandler:
         if result:
             return jsonify(self.mapToDict(result))
         else:
-            return "Not Found", 404
+            return jsonify(GetStatus="Not Found"), 404
 
     def insertRoom(self, room_json):
+        if ("building" not in room_json or "room_number" not in room_json or "capacity" not in room_json):
+            return jsonify(InsertStatus="Missing required fields"), 400
+        
         building = room_json["building"]
         room_number = room_json["room_number"]
         capacity = room_json["capacity"]
-        dao = RoomDAO()
-        rid = dao.insertRoom(building, room_number, capacity)
-        temp = (rid, building, room_number, capacity)
-        return jsonify(self.mapToDict(temp)), 201
+        
+        data = {
+            "rid": 1000,
+            "building": [building],
+            "room_number": [room_number],
+            "capacity": [capacity]
+        }
+        df_to_insert = pd.DataFrame(data)
+        df_list = clean_data(df_to_insert, "room")
+        
+        df_room = []
+        for df, df_name in df_list:
+            if df_name == "room":
+                df_room = df
+                
+        is_data_confirmed = self.confirmDataInDF(df_to_insert, df_room)
+        
+        if is_data_confirmed:
+            dao = RoomDAO()
+            rid = dao.insertRoom(building, room_number, capacity)
+            temp = (rid, building, room_number, capacity)
+            return jsonify(self.mapToDict(temp)), 201
+        else:
+            return jsonify(InsertStatus = "Data can't be inserted due to duplicates or invalid data"), 400
 
     def deleteRoomByRid(self, rid):
         dao = RoomDAO()
