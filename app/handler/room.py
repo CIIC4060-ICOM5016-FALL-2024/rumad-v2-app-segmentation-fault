f018d65dfa9a070a95c7421f319e81024e569320
JeanMontesSantiago <jean.montes@upr.edu>
2024-11-04 20:21:25 -0400

    refactor RoomHandler: streamline validation and response handling

--- app/handler/room.py
+++ app/handler/room.py
@@ -1,31 +1,16 @@
-import pandas as pd
 from flask import jsonify
 from dao.room import RoomDAO
-from handler.course import ClassHandler
 
 
 class RoomHandler:
     def mapToDict(self, tuple):
         result = {
-            "rid": tuple[0],
-            "building": tuple[1],
-            "room_number": tuple[2],
-            "capacity": tuple[3],
+            "rid" : tuple[0],
+            "building" : tuple[1],
+            "room_number" : tuple[2],
+            "capacity" : tuple[3]
         }
         return result
-
-    def confirmDataInDF(self, df_to_verify, df_meeting):
-        columns_to_check = ["building", "room_number", "capacity"]
-        df_meeting = df_meeting.astype({col: str for col in columns_to_check})
-        df_to_verify = df_to_verify.astype({col: str for col in columns_to_check})
-
-        # Check if the data to insert is already in the database
-        values_to_check = df_to_verify[columns_to_check].iloc[0]
-        duplicate_count = (
-            df_meeting[columns_to_check].eq(values_to_check).all(axis=1).sum()
-        )
-
-        return duplicate_count == 1
 
     def getAllRoom(self):
         result = []
@@ -41,29 +26,21 @@ class RoomHandler:
         if result:
             return jsonify(self.mapToDict(result))
         else:
-            return jsonify(GetStatus="Not Found"), 404
+            return "Not Found", 404
 
     def insertRoom(self, room_json):
-        if (
-            "building" not in room_json
-            or "room_number" not in room_json
-            or "capacity" not in room_json
-        ):
-            return jsonify(InsertStatus="Missing required fields"), 400
-
         building = room_json["building"]
         room_number = room_json["room_number"]
         capacity = room_json["capacity"]
 
         if not building or not room_number or not capacity:
             return jsonify(InsertStatus="Missing required fields"), 404
-        if not isinstance(capacity, int) or capacity <= 0:
+        if not isinstance(capacity, int)  or capacity <= 0:
             return jsonify(InsertStatus="Invalid capacity"), 404
-        if not isinstance(room_number, str) or len(room_number) == 0:
-            return jsonify(InsertStatus="Invalid room_number"), 404
-        if not isinstance(building, str) or len(building) == 0:
-            return jsonify(InsertStatus="Invalid building"), 404
-
+        
+        if any(len(value.strip()) == 0 or not isinstance(value, str) for value in [building, room_number]):
+            return jsonify(InsertStatus="A entry is empty or invalid type"), 400
+        
         dao = RoomDAO()
         rid = dao.insertRoom(building, room_number, capacity)
         if rid:
@@ -78,40 +55,29 @@ class RoomHandler:
         if result:
             return jsonify(DeleteStatus="OK"), 200
         else:
-            return jsonify(DeleteStatus="NOT FOUND"), 404
+            return jsonify(DeleteStatus="Not Found"), 404
 
     def updateRoomByRid(self, rid, room_json):
-        dao = RoomDAO()
-        if not dao.getRoomByRid(rid):
-            return jsonify(UpdateStatus="NOT FOUND"), 404
-
-        if (
-            "building" not in room_json
-            or "room_number" not in room_json
-            or "capacity" not in room_json
-        ):
-            return jsonify(UpdateStatus="Missing required fields"), 400
-
         building = room_json["building"]
         room_number = room_json["room_number"]
         capacity = room_json["capacity"]
 
         if not building or not room_number or not capacity:
             return jsonify(UpdateStatus="Missing required fields"), 404
-        if not isinstance(capacity, int) or capacity <= 0:
+        if not isinstance(capacity, int)  or capacity <= 0:
             return jsonify(UpdateStatus="Invalid capacity"), 404
-        if not isinstance(room_number, str) or len(room_number) == 0:
-            return jsonify(UpdateStatus="Invalid room_number"), 404
-        if not isinstance(building, str) or len(building) == 0:
-            return jsonify(UpdateStatus="Invalid building"), 404
+        
+        if any(len(value.strip()) == 0 or not isinstance(value, str) for value in [building, room_number]):
+            return jsonify(InsertStatus="A entry is empty or invalid type"), 400
 
+        
         dao = RoomDAO()
         temp = dao.updateRoomByRid(rid, building, room_number, capacity)
         if temp:
             tup = (rid, building, room_number, capacity)
             return jsonify(self.mapToDict(tup)), 200
         else:
-            return jsonify(UpdateStatus="Invalid data"), 400
+            return jsonify(UpdateStatus="Not Found"), 404
 
     def getMaxCapacity(self, building):
         result = []
@@ -124,13 +90,9 @@ class RoomHandler:
         else:
             return jsonify(UpdateStatus="Not Found"), 404
 
-    def getRatioByBuilding(self, building):
-        result = []
-        dao = RoomDAO()
-        temp = dao.getRatioByBuilding(building)
-        if temp:
-            for item in temp:
-                result.append(self.mapToDict(item))
-            return jsonify(result), 200
-        else:
-            return jsonify(UpdateStatus="Not Found"), 404
+
+
+
+
+
+
