e477ab56043b65693ddb256db3faa0fa14e410e3
JovalierOrtiz <jovalier.ortiz@upr.edu>
2024-11-18 17:44:36 -0400

    change the model execution to the cpu

--- app/vectorDB/chatBot/tokenize_class.py
+++ app/vectorDB/chatBot/tokenize_class.py
@@ -1,5 +1,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_text_splitters import SentenceTransformersTokenTextSplitter
+from sentence_transformers import SentenceTransformer
 import spacy
 
 
@@ -8,7 +9,8 @@ nlp = spacy.load("en_core_web_sm")
 class Tokenize:
     def __init__(self):
         self.separators = [" ", "\n", "\t", "\r", "\f", "\v", "\n\n", ". ",""]
-
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.model.to('cpu')
     def tokenize_text(self, text):
         # Split the text into chunks of 1000 characters
         splitter = RecursiveCharacterTextSplitter(
@@ -27,7 +29,7 @@ class Tokenize:
         for t in chararter_split_texts:
             tokens = token_split.split_text(t)
             cleaned_tokens = []
-            
+
         # Delete stop Words and Lem
             for token in tokens:
                 doc = nlp(token)
