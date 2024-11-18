a7b3d670fde1d784fcfbaa3a6223ab17d8f33195
JovalierOrtiz <jovalier.ortiz@upr.edu>
2024-11-18 17:26:29 -0400

    lemmatizer and stop worlds eraser

--- app/vectorDB/chatBot/tokenize_class.py
+++ app/vectorDB/chatBot/tokenize_class.py
@@ -1,11 +1,16 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_text_splitters import SentenceTransformersTokenTextSplitter
+import spacy
+
+
+nlp = spacy.load("en_core_web_sm")
 
 class Tokenize:
     def __init__(self):
         self.separators = [" ", "\n", "\t", "\r", "\f", "\v", "\n\n", ". ",""]
 
     def tokenize_text(self, text):
+        # Split the text into chunks of 1000 characters
         splitter = RecursiveCharacterTextSplitter(
             separators=self.separators,
             chunk_size=1000,
@@ -13,12 +18,21 @@ class Tokenize:
         )
         chararter_split_texts = splitter.split_text(text)
 
+        # First tokenize the text 
         token_split = SentenceTransformersTokenTextSplitter(
             chunk_overlap=0,
             tokens_per_chunk=256
         )
         token_split_texts = []
         for t in chararter_split_texts:
-            token_split_texts += token_split.split_text(t)
+            tokens = token_split.split_text(t)
+            cleaned_tokens = []
+            
+        # Delete stop Words and Lem
+            for token in tokens:
+                doc = nlp(token)
+                words = [token.lemma_ for token in doc if not token.is_stop]
+                cleaned_tokens.append(" ".join(words))
+            token_split_texts.extend(cleaned_tokens)
         
         return token_split_texts
