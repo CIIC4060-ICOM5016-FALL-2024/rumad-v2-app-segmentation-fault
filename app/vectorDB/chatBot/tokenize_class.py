126d617f9edb4e483414dba0d7a1e41d55c25b31
JovalierOrtiz <jovalier.ortiz@upr.edu>
2024-11-18 19:03:32 -0400

    add comments to tokenize class

--- app/vectorDB/chatBot/tokenize_class.py
+++ app/vectorDB/chatBot/tokenize_class.py
@@ -3,16 +3,20 @@ from langchain_text_splitters import SentenceTransformersTokenTextSplitter
 from sentence_transformers import SentenceTransformer
 import spacy
 
-
+# Load a spaCy model
 nlp = spacy.load("en_core_web_sm")
 
 class Tokenize:
     def __init__(self):
         self.separators = [" ", "\n", "\t", "\r", "\f", "\v", "\n\n", ". ",""]
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
-        self.model.to('cpu')
+
     def tokenize_text(self, text):
-        # Split the text into chunks of 1000 characters
+
+        # List to store the cleaned tokens (without stop words and lemmatized)
+        cleaned_tokens = []
+
+        # Split the text (Chunks of 1000 characters)
         splitter = RecursiveCharacterTextSplitter(
             separators=self.separators,
             chunk_size=1000,
@@ -20,21 +24,27 @@ class Tokenize:
         )
         chararter_split_texts = splitter.split_text(text)
 
-        # First tokenize the text 
+        # Split the text into tokens (256 tokens per chunk) 
         token_split = SentenceTransformersTokenTextSplitter(
             chunk_overlap=0,
             tokens_per_chunk=256
         )
-        token_split_texts = []
+
         for t in chararter_split_texts:
             tokens = token_split.split_text(t)
-            cleaned_tokens = []
+    
 
-        # Delete stop Words and Lem
+            # Apply the spacy model to obtain (stop words, base verbs)
             for token in tokens:
                 doc = nlp(token)
-                words = [token.lemma_ for token in doc if not token.is_stop]
-                cleaned_tokens.append(" ".join(words))
-            token_split_texts.extend(cleaned_tokens)
+
+                # Delete stop Words
+                for token in doc:
+                    if not token.is_stop:
+                        cleaned_tokens.append(token)
+                
+                # Lemmatize the verbs (Change the verbs to base form)
+                cleaned_tokens = [token.lemma_ for token in cleaned_tokens]
+
         
-        return token_split_texts
+        return cleaned_tokens
