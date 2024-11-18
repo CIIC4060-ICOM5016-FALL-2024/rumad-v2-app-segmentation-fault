d78a0da444d96a238982e1233a87176372e1a609
Onnelle Lugo <onnelle.lugo@upr.edu>
2024-11-18 08:29:48 -0400

    change cred to db practice

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,7 +1,7 @@
 pg_config = {
-    "user": "ubj1eoeaku13t6",
-    "password": "pdef0051a9c37a2f131b786f09aaeb192dffcbe140fe7d048dcc6c62342c3dbcc",
-    "dbname": "d4i4fjj12gdddt",
-    "host": "c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
+    "user": "u5idsnjdesf6q6",
+    "password": "p89b56b039e881b39037ce3337b1b29e699c39ffd4862a4edd166176211f69a2a",
+    "dbname": "ddau5ns0kgvq0k",
+    "host": "ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
     "port": "5432",
 }
