eed56557aeb684e654e380342186e7fadd8448d8
JovalierOrtiz <jovalier.ortiz@upr.edu>
2024-11-18 23:59:02 -0400

    add logic to insert syllabus in filehandler

--- app/vectorDB/chatBot/filehandler.py
+++ app/vectorDB/chatBot/filehandler.py
@@ -1,73 +1,49 @@
 import sys
 import os
-from os.path import join
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
-from os import listdir
-from dao.syllabus import SyllabusDao
-from dao.fragments import FragmentsDAO
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+import numpy as np
+from dao.course import ClassDAO
+from dao.syllabus import SyllabusDAO
 from tokenize_class import Tokenize
+from embedding import embeddingClass
 import subprocess
 
 # Run the extract_pdf.py script
-result1 = subprocess.run(["python", "./app/vectorDB/dao/extract_pdf.py"], capture_output=True, text=True)
+#result1 = subprocess.run(["python", "./app/vectorDB/dao/extract_pdf.py"], capture_output=True, text=True)
 
-
-files = listdir("./extracted_syllabuses")
-
-#extract chunks
-syllabusDao = SyllabusDao()
-fragmentsDAO = FragmentsDAO()
+# Initialize the DAOs and folder path
+syllabusDao = SyllabusDAO()
 tokenize = Tokenize()
-for f in files:
+emb = embeddingClass()
+class_Dao = ClassDAO()
+folder_path = "./extracted_syllabuses"
+
+def normalizer(vector):
+    vector = np.array(vector)
+    padded_vector = np.pad(vector,pad_width=(0,500 - len(vector)), mode='constant')
+    return padded_vector
+
+
+# Iterate over the extracted syllabus files (.txt)
+for f in os.listdir(folder_path):
     print(f"\033[93mWorking with: {f}\033[0m")
-    file_path = join("./extracted_syllabuses", f)
+
+    file_path = os.path.join(folder_path, f)
+    count = 0
     with open(file_path, "r") as file:
         text = file.read()
-        tokenized = tokenize.tokenize_text(text)
-        print(tokenized)
+        text = tokenize.tokenize_text(text)
+
+        for actual_chunk in text:
+            count += 1
+            print(f"\033[94mChunk {count}\033[0m")
+            embText = normalizer(emb.embed(actual_chunk[0])).tolist()
+            print(actual_chunk)
+            # Insert syllabus into the database
+            cid = class_Dao.getClassByCname_Ccode(f.split("-")[0], f.split("-")[1])[0]
+            chunkid = syllabusDao.insertSyllabus(cid, embText, actual_chunk)
+
         print(f"\033[92m{f} done \n\n\033[0m")
-        
-    '''chunkid = syllabusDao.insertSyllabus()
-        for t in tokenized:
-            emb = model.encode(t)
-            fragmentsDAO.insertFragment(did, t, emb.tolist())'''
-'''
-    #split
-    character_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n", ". ", " ", ""],
-        chunk_size=100,
-        chunk_overlap=50
-    )
-    character_split_texts = character_splitter.split_text('\n\n'.join(pdf_texts))
 
-    print(character_split_texts[10])
-    print(f"\nTotal chunks: {len(character_split_texts)}")
-
-    [print(t) for t in character_split_texts]
-    print()
-    #Token
-    token_splitter = SentenceTransformersTokenTextSplitter(chunk_overlap=50, tokens_per_chunk=256)
-
-    token_split_texts = []
-    for text in character_split_texts:
-        token_split_texts += token_splitter.split_text(text)
-    print("token")
-    print(token_split_texts[10])
-    [print(t) for t in token_split_texts]
-    print(f"\nTotal Splitted chunks: {len(token_split_texts)}")
-    #exit(1)
-
-    # insert document into table
-    did = docDAO.insertDoc(f)
-
-
-    for t in token_split_texts:
-        emb = model.encode(t)
-        #print(t)
-        #print(emb)
-        fraDAO.insertFragment(did, t, emb.tolist())
-
-    print("Done file: " + f)
-'''
 
 
