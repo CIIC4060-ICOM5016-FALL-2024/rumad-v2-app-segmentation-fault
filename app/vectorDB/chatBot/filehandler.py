b4a975a31546b7138f64afee588b19d0c2827660
JovalierOrtiz <jovalier.ortiz@upr.edu>
2024-11-19 13:27:40 -0400

    fix memory collapse

--- app/vectorDB/chatBot/filehandler.py
+++ app/vectorDB/chatBot/filehandler.py
@@ -7,6 +7,7 @@ from dao.syllabus import SyllabusDAO
 from tokenize_class import Tokenize
 from embedding import embeddingClass
 import subprocess
+import gc
 
 # Run the extract_pdf.py script
 #result1 = subprocess.run(["python", "./app/vectorDB/dao/extract_pdf.py"], capture_output=True, text=True)
@@ -25,25 +26,34 @@ def normalizer(vector):
 
 
 # Iterate over the extracted syllabus files (.txt)
-for f in os.listdir(folder_path):
+folder = os.listdir(folder_path)
+count = 0
+for f in folder:
+    count += 1
+    print("File: " "%s/%s" % (count, len(folder)))
     print(f"\033[93mWorking with: {f}\033[0m")
 
     file_path = os.path.join(folder_path, f)
-    count = 0
     with open(file_path, "r") as file:
         text = file.read()
         text = tokenize.tokenize_text(text)
 
         for actual_chunk in text:
-            count += 1
-            print(f"\033[94mChunk {count}\033[0m")
             embText = normalizer(emb.embed(actual_chunk[0])).tolist()
-            print(actual_chunk)
             # Insert syllabus into the database
             cid = class_Dao.getClassByCname_Ccode(f.split("-")[0], f.split("-")[1])[0]
-            chunkid = syllabusDao.insertSyllabus(cid, embText, actual_chunk)
+            syllabusDao.insertSyllabus(cid, embText, actual_chunk)
+            del embText
+            del cid
+            del actual_chunk
+            gc.collect()
 
-        print(f"\033[92m{f} done \n\n\033[0m")
+        
+
+    print(f"\033[34m{f} chunks insertion:\033[92m done \n\033[0m")
+
+folder.clear()
+        
 
 
 
