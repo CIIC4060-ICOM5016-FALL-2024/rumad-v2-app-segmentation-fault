857cd4e965c3a05ed7e07c99beb4626822feb639
Onnelle Lugo <onnelle.lugo@upr.edu>
2024-11-23 15:16:43 -0400

    refactor: simplify tokenization logic and remove spaCy dependency

--- app/vectorDB/chatBot/tokenize_class.py
+++ app/vectorDB/chatBot/tokenize_class.py
@@ -1,13 +1,10 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_text_splitters import SentenceTransformersTokenTextSplitter
-import spacy
 
-# Load a spaCy model
-nlp = spacy.load("en_core_web_sm")
 
 class Tokenize:
     def __init__(self):
-        self.separators = [" ", "\n", "\t", "\r", "\f", "\v", "\n\n", ". ",""]
+        self.separators = [" ", "\n", "\t", "\r", "\f", "\v", "\n\n", ". ", ""]
 
     def tokenize_text(self, text):
 
@@ -16,19 +13,15 @@ class Tokenize:
 
         # Split the text (Chunks of 1000 characters)
         splitter = RecursiveCharacterTextSplitter(
-            separators=self.separators,
-            chunk_size=600,
-            chunk_overlap=0
+            separators=self.separators, chunk_size=600, chunk_overlap=0
         )
         character_split_texts = splitter.split_text(text)
 
-        # Split the text into tokens (256 tokens per chunk) 
+        # Split the text into tokens (256 tokens per chunk)
         token_split = SentenceTransformersTokenTextSplitter(
-            chunk_overlap=0,
-            tokens_per_chunk=1
+            chunk_overlap=0, tokens_per_chunk=1
         )
 
-        
         character_split_texts = splitter.split_text(text)
         for chunk in character_split_texts:
             token_chunks = token_split.split_text(chunk)
