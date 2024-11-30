1a4d2aae1ddf31082e62caa37c90b8d31dd21eaa
JeanMontesSantiago <jean.montes@upr.edu>
2024-11-29 22:16:51 -0400

    Enhance Local Statistics page with improved layout, dynamic building selection, and error handling for API requests

--- app/streamlitApp/pages/Local_Statistics.py
+++ app/streamlitApp/pages/Local_Statistics.py
@@ -5,6 +5,13 @@ import requests
 from matplotlib import colors as mcolors
 import io
 
+st.set_page_config(
+    page_title="Local Stats",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    page_icon="./logos/seal-rum-uprm-1280x1280px.png"
+) 
+
 # inyect CSS to style the page
 st.markdown(
     """
@@ -57,7 +64,7 @@ st.markdown(
 
 def generate_green_shades(base_color, values):
     base_rgb = mcolors.hex2color(base_color)
-    lighter_rgb = [(c + 1) / 2 for c in base_rgb]  # A lighter version of the base green
+    lighter_rgb = [(c + 1) / 2 for c in base_rgb]  
     colors = [
         mcolors.to_hex([(1 - value) * light + value * dark for light, dark in zip(lighter_rgb, base_rgb)])
         for value in values
@@ -65,6 +72,11 @@ def generate_green_shades(base_color, values):
     return colors
 
 st.title("Local Stats")
+
+st.markdown("""
+This page offers an intuitive platform for exploring local data insights. Dropdown menus dynamically update with the latest data, ensuring a seamless and accurate experience. Analyze key statistics through interactive charts, including the top rooms by capacity, the highest student-to-capacity ratios, and the most-taught classes by semester and room. 
+""")
+
 
 top_three_rooms_per_capacity_container = st.container()
 top_three_rooms_per_ration_container = st.container()
@@ -78,21 +90,32 @@ if st.session_state.get("login"):
         st.subheader("Top 3 rooms with the most capacity")
         st.divider()
         building = "Stefani"
-        buildings = ["Stefani", "Software", "Monzon"]
-        for i, col in enumerate(st.columns(3)):
-            with col:
-                if st.button(buildings[i], key=f"capacity_button_{buildings[i]}"):
-                    building = buildings[i]
+        buildings = []
+        try:
+            response = requests.get(f"https://rumad-db-5dd7ab118ab8.herokuapp.com/segmentation_fault/room")
+            df = pd.json_normalize(response.json())
+            buildings = (df["building"].unique()).tolist()
+        
+        except Exception as e:
+            st.write(f"Error: Could not connect to the API. {e}")
+
+        building = st.selectbox("Select a building", buildings, key="building_capacity")
+
         try:
             response = requests.post(f"https://rumad-db-5dd7ab118ab8.herokuapp.com/segmentation_fault/room/{building}/capacity")
             if response.status_code == 200:
                 data = response.json()
                 df = pd.json_normalize(data)
 
-                df["normalized_capacity"] = (df["capacity"] - df["capacity"].min()) / (df["capacity"].max() - df["capacity"].min())
-                df["colors"] = generate_green_shades("#327136", df["normalized_capacity"])
+                epsilon = 1e-10  # A small value to prevent division by zero
+                df["normalized_capacity"] = (df["capacity"] - df["capacity"].min()) / (df["capacity"].max() - df["capacity"].min() + epsilon)
 
+                if not df["normalized_capacity"].isna().any():
+                    df["colors"] = generate_green_shades("#327136", df["normalized_capacity"])
+                else:
+                    df["colors"] = generate_green_shades("#327136", df["capacity"])
 
+            
                 fig_stacked = px.bar(
                     df,
                     x="capacity",
@@ -101,7 +124,7 @@ if st.session_state.get("login"):
                     color_discrete_sequence=df["colors"],  
                     labels={"Capacity": "Room Capacity", "Building": "Building Name"},
                     orientation="h",
-                )
+                    )
                 fig_stacked.update_layout(
                     xaxis=dict(
                         showline=True,  # Show boundary line for x-axis
@@ -130,11 +153,16 @@ if st.session_state.get("login"):
         st.subheader("Top 3 sections with the most student-to-capacity ratio.")
         st.divider()
         building = "Stefani"
-        buildings = ["Stefani", "Software", "Monzon"]
-        for i, col in enumerate(st.columns(3)):
-            with col:
-                if st.button(buildings[i], key=f"ratio_button_{buildings[i]}"):
-                    building = buildings[i]
+        buildings = []
+        try:
+            response = requests.get(f"https://rumad-db-5dd7ab118ab8.herokuapp.com/segmentation_fault/room")
+            df = pd.json_normalize(response.json())
+            buildings = (df["building"].unique()).tolist()
+        
+        except Exception as e:
+            st.write(f"Error: Could not connect to the API. {e}")
+
+        building = st.selectbox("Select a building", buildings, key="building_ratio")
 
         try:
             response = requests.post(f"https://rumad-db-5dd7ab118ab8.herokuapp.com/segmentation_fault/room/{building}/ratio")
@@ -143,7 +171,6 @@ if st.session_state.get("login"):
                 df = pd.json_normalize(data)
 
                 df["colors"] = generate_green_shades("#327136", df["ratio"])
-
 
                 fig_stacked = px.bar(
                     df,
@@ -176,7 +203,7 @@ if st.session_state.get("login"):
             else:
                 st.write("Error: " + response.status_code)
         except:
-            st.write("Error: Could not connect to the API.")
+            st.write("This building has no section.")
 
     with top_three_classes_taught_per_semester_container:
         st.subheader("Top 3 most taught classes per semester.")
@@ -197,6 +224,45 @@ if st.session_state.get("login"):
             response = requests.post(f"https://rumad-db-5dd7ab118ab8.herokuapp.com/segmentation_fault/classes/{year}/{semester}")
             if response.status_code == 200:
                 data = response.json()
+                df = pd.json_normalize(data)
+
+                df['normalized_class_count'] = (df['class_count'] - df['class_count'].min()) / (df['class_count'].max() - df['class_count'].min())
+                df["cname_ccode"] = df["cname"] + df["ccode"]
+
+                if not df["normalized_class_count"].isna().any():
+                    df["colors"] = generate_green_shades("#327136", df["normalized_class_count"])
+                else:
+                    df["colors"] = generate_green_shades("#327136", df["class_count"])
+
+                fig_stacked = px.bar(
+                    df,
+                    x="class_count",
+                    y="cname_ccode",
+                    color="cname_ccode",
+                    color_discrete_sequence=df["colors"],  
+                    labels={"Capacity": "Room Capacity", "Building": "Building Name"},
+                    orientation="h",
+                )
+                fig_stacked.update_layout(
+                    xaxis=dict(
+                        showline=True,  # Show boundary line for x-axis
+                        linewidth=2,  # Line width
+                        linecolor="black",  # Line color
+                        showgrid=True,  # Enable gridlines
+                        gridcolor="lightgray",  # Gridline color
+                        gridwidth=0.5,  # Gridline width
+                    ),
+                    yaxis=dict(
+                        type="category",
+                        categoryorder="total ascending",
+                        showline=True,  # Show boundary line for y-axis
+                        linewidth=2,  # Line width
+                        linecolor="black",  # Line color
+                    ),
+                    plot_bgcolor="white",  # Set background color to white
+                )
+                st.plotly_chart(fig_stacked)
+
                 for i, class_info in enumerate(data):
                     st.markdown(f"""
                         <h3>Class {i + 1}: {class_info["cdesc"]}</h3>
@@ -281,6 +347,46 @@ if st.session_state.get("login"):
                 response = requests.post(f"https://rumad-db-5dd7ab118ab8.herokuapp.com/segmentation_fault/room/{selected_rid}/classes")
                 if response.status_code == 200:
                     data = response.json()
+                    df = pd.json_normalize(data)
+
+                    df['normalized_class_count'] = (df['class_count'] - df['class_count'].min()) / (df['class_count'].max() - df['class_count'].min())
+                    df["cname_ccode"] = df["cname"] + df["ccode"]
+
+                    if not df["normalized_class_count"].isna().any():
+                        df["colors"] = generate_green_shades("#327136", df["normalized_class_count"])
+                    else:
+                        df["colors"] = generate_green_shades("#327136", df["class_count"])
+
+                    fig_stacked = px.bar(
+                        df,
+                        x="class_count",
+                        y="cname_ccode",
+                        color="cname_ccode",
+                        color_discrete_sequence=df["colors"],
+                        labels={"Capacity": "Room Capacity", "Building": "Building Name"},
+                        orientation="h",
+                    )
+                    fig_stacked.update_layout(
+                        xaxis=dict(
+                            showline=True,  # Show boundary line for x-axis
+                            linewidth=2,  # Line width
+                            linecolor="black",  # Line color
+                            showgrid=True,  # Enable gridlines
+                            gridcolor="lightgray",  # Gridline color
+                            gridwidth=0.5,  # Gridline width
+                        ),
+                        yaxis=dict(
+                            type="category",
+                            categoryorder="total ascending",
+                            showline=True,  # Show boundary line for y-axis
+                            linewidth=2,  # Line width
+                            linecolor="black",  # Line color
+                        ),
+                        plot_bgcolor="white",  # Set background color to white
+                    )
+
+                    st.plotly_chart(fig_stacked)
+
                     for i, class_info in enumerate(data):
                         st.markdown(f"""
                             <h3>Class {i + 1}: {class_info["cdesc"]}</h3>
@@ -351,4 +457,5 @@ if st.session_state.get("login"):
 else:
     st.error("You need to login first to access this page.")
         
+#327136
 
