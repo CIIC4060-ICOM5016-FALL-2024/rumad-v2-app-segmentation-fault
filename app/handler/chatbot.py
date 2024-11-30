de5cedc4e4564d8624b9cba836bfc134fb9d417d
JeanMontesSantiago <jean.montes@upr.edu>
2024-11-29 22:45:00 -0400

    Update chatbot URL for segmentation fault handling

--- app/handler/chatbot.py
+++ app/handler/chatbot.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from dao.syllabus import SyllabusDAO
 from dao.course import ClassDAO
 
-url = "https://c2d9-24-55-160-197.ngrok-free.app/segmentation_fault/"
+url = "https://e846-24-55-160-197.ngrok-free.app/segmentation_fault/"
 
 
 class ChatbotHandler:
